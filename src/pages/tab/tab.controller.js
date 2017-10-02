51dac8bb8f33c34329a06b39eec1ab33c115672e
johnnynode <johnnynode@gmail.com>
2017-10-02 19:06:25 +0800

    add tab module

---
+++ src/pages/tab/tab.controller.js
@@ -0,0 +1,9 @@
+(function (angular) {
+    'use strict';
+    angular.module('happyLearning')
+        .controller('TabCtrl', [
+            '$scope',
+            function ($scope) {
+            // 初始化数据成员
+        }]);
+})(angular);
---
+++ src/pages/tab/tab.route.js
@@ -0,0 +1,13 @@
+(function (angular) {
+  'use strict';
+  angular.module('happyLearning')
+    .config(function ($stateProvider) {
+      $stateProvider
+        .state('tab', {
+          url: '/tab',
+          abstract: true,
+          templateUrl: 'pages/tab/tab.html',
+          controller: 'TabCtrl'
+        })
+    });
+})(angular);
