266a7c52c8ff1119310e1d12c4d23d8f3bdb85f0
johnnynode <johnnynode@gmail.com>
2017-10-05 18:33:04 +0800

    change mock data to http request way

--- src/pages/home/home.controller.js
+++ src/pages/home/home.controller.js
@@ -5,7 +5,8 @@
       '$scope',
       '$ionicSlideBoxDelegate',
       '$timeout',
-      function ($scope, $ionicSlideBoxDelegate, $timeout) {
+      '$http',
+      function ($scope, $ionicSlideBoxDelegate, $timeout, $http) {
         /* 初始化数据成员 */
         var fn = $scope.fn = {};
         var viewData = $scope.viewData = {}; // 视图数据绑定
@@ -38,26 +39,21 @@
           });
         }
 
-        // 轮播下的课程类别 mock fake data
+        // 轮播下的课程类别
         function getCategory() {
-          viewData.scrollCate = [
-            {name: "互联网产品", code: "00001"},
-            {name: "编程语言", code: "00002"},
-            {name: "移动开发", code: "00003"},
-            {name: "软件测试", code: "00004"},
-            {name: "云计算", code: "00005"},
-            {name: "大数据", code: "00006"},
-            {name: "游戏开发", code: "00007"},
-            {name: "IT认证", code: "00008"},
-            {name: "网络营销", code: "00009"},
-            {name: "兴趣生活", code: "00010"}
-          ];
+          $http.get("./data/home.scrollCate.json")
+            .success(function (data) {
+              viewData.scrollCate = data;
+            })
+            .error(function (e) {
+              console.log(e);
+            });
         }
 
         // 每次滑动到当前模块,执行一个动画
         fn.slideHasChanged = function (index) {
           // todo
-          console.log(index)
+          console.log(index);
         };
 
       }]);
